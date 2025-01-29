4d7d8f228e59aff91a9dc49d6abcc950cd4a99ca
balakrishnanthirumal <balakrishnanthirumalt@gmail.com>
2025-01-29 15:26:33 +0530

    Done

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/authMiddleware.js";
-import { getAllUsers } from "../controllers/userController.js";
+import { getAllUsers, getMessages } from "../controllers/userController.js";
 
 const router = Router();
 
 router.get("/", protectRoute, getAllUsers);
+
+router.get("/messages/:userId", protectRoute, getMessages);
 
 export default router;
